1e527164f8dfcfbd319603a491269ea3af4ca36a
Dan Vuong <h.dan.vuong@gmail.com>
2019-03-24 06:08:37 +0100

    alert button send email

--- dashboard/src/app/card-zone/card-zone.component.ts
+++ dashboard/src/app/card-zone/card-zone.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {DataBasic} from '../models/data-basic.model'
+import { HttpHeaders } from '@angular/common/http';
 
 
 @Component({
@@ -11,10 +11,11 @@ import {DataBasic} from '../models/data-basic.model'
 export class CardZoneComponent implements OnInit {
 
   temperature = 20;
-  temperature_weather = 22;
+  temperature_weather = 19;
+  weather_text = "few clouds";
   humidity;
   light = 80;
-  wind = 35;
+  wind = 5;
   days = [{"name":"M","status":"not"}, {"name":"T","status":"not"}, {"name":"W","status":"not"}, {"name":"T","status":"not"}, {"name":"F","status":"not"}, {"name":"S","status":"not"}, {"name":"S","status":"red"}]
 
   constructor(private http : HttpClient) { }
@@ -35,6 +36,7 @@ export class CardZoneComponent implements OnInit {
 
   getData() {
     this.http.get('http://192.168.43.111:8080/getData')
+    //this.http.get('http://192.168.43.80:8080/getData') // merlin
     //this.http.get('http://192.168.56.203:8080/getData')
       .subscribe(res => {
           console.log(res);
@@ -44,11 +46,34 @@ export class CardZoneComponent implements OnInit {
       });
 
     this.http.get('http://192.168.43.111:8080/weather')
+    //this.http.get('http://192.168.43.80:8080/weather') //tel merlin
    //this.http.get('http://192.168.56.203:8080/weather')
       .subscribe(res =>{
         console.log(res);
-        this.temperature_weather = (res as any).temp;
-        this.wind = (res as any).wind;
+        this.temperature_weather = Math.floor((res as any).temp);
+        this.wind = Math.floor((res as any).wind);
+        this.weather_text = (res as any).weather;
       });
+  }
+
+  clickAlert() {
+    console.log('alert');
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'my-auth-token'
+      })
+    };
+
+    const json = {"type" : "coucou"};
+
+    //this.http.post('http://192.168.56.203:8080/sendmail', json, httpOptions)
+    //this.http.post('http://192.168.43.80:8080/sendmail', json, httpOptions)
+    this.http.post('http://192.168.43.111:8080/sendmail', json)
+      .subscribe(val => {
+        console.log(val);
+      })
+
   }
 }
